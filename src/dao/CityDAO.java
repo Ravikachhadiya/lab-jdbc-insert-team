a64882a338d81a3238d539830be96475a3326799
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-10-22 18:05:21 +0530

    Inesert Team

--- src/dao/CityDAO.java
+++ src/dao/CityDAO.java
@@ -1,1 +1,48 @@
+package dao;
 
+import java.sql.Statement;
+
+import model.City;
+import utility.ConnectionManager;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CityDAO{
+	
+	public void getDetails() throws SQLException, ClassNotFoundException, IOException
+	{
+		Connection con = ConnectionManager.getConnection();
+		Statement st =  con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM TEAM");
+		while(rs.next())
+		{
+			System.out.print(rs.getString(1)+"\t ");
+			System.out.print(rs.getString(2)+"\t ");
+			System.out.println(rs.getString(5));
+		}
+
+	}
+	
+	public void getCityByName(String name) throws SQLException, ClassNotFoundException, IOException
+	{
+		Connection con = ConnectionManager.getConnection();
+		Statement stmt=  con.createStatement();
+		ResultSet rs =stmt.executeQuery("SELECT * FROM TEAM");
+		while(rs.next())
+		{
+			if(name.equals(rs.getString("CITY")))
+			{
+			System.out.println("Team Name : "+rs.getString(1));
+			System.out.println("Coach Name : "+rs.getString(2));
+			System.out.println("Captain Name : "+rs.getString(3));
+			System.out.println("City ID : "+rs.getLong(4));
+			System.out.println("City Name : "+rs.getString(5));
+
+			}
+		}
+
+	}
+}
--- src/dao/TeamDAO.java
+++ src/dao/TeamDAO.java
@@ -1,1 +1,35 @@
 
+package dao;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.City;
+import model.Team;
+import utility.ConnectionManager;
+
+public class TeamDAO
+{
+	public void createTeam(Team team) throws ClassNotFoundException, SQLException, IOException 
+	{
+		Connection con = ConnectionManager.getConnection();
+
+		String sql = "INSERT INTO TEAM(id, name ,coach,captain,cityid)VALUES(?,?,?,?,?)";
+		PreparedStatement st = con.prepareStatement(sql);
+		City city = (City) team.getCity();
+
+		st.setInt(1, 5);
+		st.setString(2, team.getName());
+		st.setString(3,team.getCoach());
+		st.setString(4,team.getCaptain());
+		st.setLong(5,city.getCityId());
+    	
+
+		st.executeUpdate();
+		con.close();
+		System.out.println("Team has been created");
+	}
+}
